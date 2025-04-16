2d096c1f9122ad39fe99d0f2bd66787485cd5832
basar <basarsubasi@protonmail.com>
2025-04-17 02:52:21 +0300

    shadowing and scope

--- The Rust Book/variables/src/main.rs
+++ The Rust Book/variables/src/main.rs
@@ -2,5 +2,17 @@ fn main() {
     let mut x = 5;
     println!("the value of x is {x}");
     x = 6;
-    println!("the value of x is {x}")
+    println!("the value of x is {x}");
+
+    let y = 5;
+
+    let y = y+1;
+
+    {
+        let y = y*2;
+
+        println!("the value of y is {y}");
+    }
+
+    println!("the value of y is {y}");
 }
