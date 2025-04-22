85e41089ed26535e2619b17feaf85ca3323fa994
basar <basarsubasi@protonmail.com>
2025-04-22 03:00:28 +0300

    loops

--- The Rust Book/control_flow/src/main.rs
+++ The Rust Book/control_flow/src/main.rs
@@ -2,14 +2,14 @@ fn main() {
     let number: i32 = 7;
 
     if number < 6 {
-        println!("number is smaller than 6")
+        println!("number is smaller than 6");
     } else {
-        println!("number is not smaller than 6")
+        println!("number is not smaller than 6");
     }
 
     let condition:bool = false;
 
     let x:i32 = if condition {1} else {2};
 
-    println!("{x}")
+    println!("{x}");
 }
